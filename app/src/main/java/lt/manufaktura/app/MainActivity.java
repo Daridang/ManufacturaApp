82d40fd7237d5dfeb23d2b66509b5f9ab0bb73e8
Daridang <daridang@gmail.com>
2020-10-02 21:13:20 +0300

    app center analytics

--- app/src/main/java/lt/manufaktura/app/MainActivity.java
+++ app/src/main/java/lt/manufaktura/app/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
@@ -14,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "a75e07f2-554e-4b28-aabd-1b271912637a",
+                Analytics.class, Crashes.class);
     }
 }
